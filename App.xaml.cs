59babc9cb18ff973fca925e61f12e7f77c440022
vijayarasan <58214164+vijayarasan@users.noreply.github.com>
2021-05-31 22:35:11 +0530

    Sample Added
    Sample Added

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace SfDataGridDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
